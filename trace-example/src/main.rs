7163c7db7c9073c05ed912fc3d5d48706902aa3a
mengyi <1203669692@qq.com>
2022-02-25 18:36:51 +0800

    优化代码

--- trace-example/src/main.rs
+++ trace-example/src/main.rs
@@ -8,24 +8,48 @@ use tracing_subscriber::fmt::writer::MakeWriterExt;
 
 use time::{macros::format_description, UtcOffset};
 use tracing_subscriber::fmt::time::OffsetTime;
-use tracing_subscriber::EnvFilter;
+// use tracing_subscriber::EnvFilter;
 
-#[tokio::main]
-async fn main() {
-    // Set the RUST_LOG, if it hasn't been explicitly defined
+use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
+
+//pub fn init_log(info_appender: NonBlocking, warn_appender: NonBlocking) {
+pub struct Logger {
+    info_appender: NonBlocking,
+    _info_guard: WorkerGuard,
+    warn_appender: NonBlocking,
+    _warn_guard: WorkerGuard,
+}
+
+impl Logger {
+    pub fn new() -> Logger {
+        let info_appender = rolling::daily("/opt/logs", "info");
+        let (info_appender, _info_guard) = tracing_appender::non_blocking(info_appender);
+        let warn_appender = rolling::daily("/opt/logs", "warn");
+        let (warn_appender, _warn_guard) = tracing_appender::non_blocking(warn_appender);
+
+        Logger {
+            info_appender,
+            _info_guard,
+            warn_appender,
+            _warn_guard,
+        }
+    }
+}
+
+pub fn init_log(logger: Logger) {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "trace-example=debug,tower_http=debug")
     }
 
-    let info_appender = rolling::daily("/opt/logs", "info");
-    let (info_appender, _info_guard) = tracing_appender::non_blocking(info_appender);
+    // let info_appender = rolling::daily("/opt/logs", "info");
+    // let (info_appender, _info_guard) = tracing_appender::non_blocking(info_appender);
+    // let warn_appender = rolling::daily("/opt/logs", "warn");
+    // let (warn_appender, _warn_guard) = tracing_appender::non_blocking(warn_appender);
 
-    let warn_appender = rolling::daily("/opt/logs", "warn");
-    let (warn_appender, _warn_guard) = tracing_appender::non_blocking(warn_appender);
-
-    let mk_writer = info_appender
+    let mk_writer = logger
+        .info_appender
         .with_min_level(Level::INFO)
-        .and(warn_appender.with_max_level(Level::WARN))
+        .and(logger.warn_appender.with_max_level(Level::WARN))
         .and(std::io::stdout.with_max_level(tracing::Level::TRACE));
 
     let local_time = OffsetTime::new(
@@ -34,13 +58,25 @@ async fn main() {
     );
 
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
+        // .with_env_filter(EnvFilter::from_default_env())
         .with_writer(mk_writer)
         .with_max_level(Level::TRACE)
         .with_timer(local_time)
         .init();
+}
 
+#[tokio::main]
+async fn main() {
+    // Set the RUST_LOG, if it hasn't been explicitly defined
     // build our application with a route
+
+    // let info_appender = rolling::daily("/opt/logs", "info");
+    // let (info_appender, _info_guard) = tracing_appender::non_blocking(info_appender);
+    // let warn_appender = rolling::daily("/opt/logs", "warn");
+    // let (warn_appender, _warn_guard) = tracing_appender::non_blocking(warn_appender);
+    let logger = Logger::new();
+    init_log(logger);
+
     let app = Router::new()
         .route("/", get(handler))
         .layer(TraceLayer::new_for_http());
